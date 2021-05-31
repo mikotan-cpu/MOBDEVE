79869587294627587573a08fa1d2e631e55a1e43
mikotan-cpu <68640929+mikotan-cpu@users.noreply.github.com>
2021-05-31 12:46:58 +0800

    Update DBHelper.java

--- Sukat/app/src/main/java/com/example/SukatApp/DBHelper.java
+++ Sukat/app/src/main/java/com/example/SukatApp/DBHelper.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
+//This java class is responsible for all DB-related functions
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context) {
         super(context, "Measurements.db", null, 1);
@@ -23,6 +25,13 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
+    /**
+     * Inserts an entry to the database with the following data:
+     * @param name of the object to be inserted
+     * @param dimension of the object to be measured(Height or Width)
+     * @param numerical measurement of the object's dimension.
+     * @param unit of measurement
+     */
     public Boolean insertMeasurement(String ObjectName, String dimension, float measurement, String unit) {
         SQLiteDatabase DB = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -41,31 +50,10 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
-    public Boolean updateMeasurement(String ObjectName, String dimension, float measurement, String unit) {
-        SQLiteDatabase DB = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("ObjectName", ObjectName);
-        contentValues.put("dimension", dimension);
-        contentValues.put("measurement", measurement);
-        contentValues.put("unit", unit);
-
-        Cursor cursor = DB.rawQuery("Select * from Measurements where ObjectName = ?", new String[]{ObjectName});
-        if (cursor.getCount() > 0) {
-
-
-            long result = DB.update("Measurements", contentValues, "ObjectName", new String[]{ObjectName});
-
-            if (result == -1) {
-                return false;
-            } else {
-                return true;
-            }
-        }else {
-            return false;
-        }
-
-    }
-
+   /**
+     * Deletes an entry from the database
+     * @param name of the object to be deleted
+     */
     public Boolean deleteMeasurement(String ObjectName) {
         SQLiteDatabase DB = this.getWritableDatabase();
 
